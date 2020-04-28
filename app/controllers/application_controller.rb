e5ed0f512790bd4429f7d02421d9f3c24e02440f
Evan <emccullough@covermymeds.com>
2020-04-27 23:28:48 -0400

    troubleshooting bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def set_user_island_path
     return unless user_signed_in?
 
-    user_island = Island.find_by(owner: current_user.id)
-    @user_island_path = user_island.nil? ? new_island_path : edit_island_path(user_island.id)
+    user_island = Island.find_by(user: current_user)
+    @user_island_path = user_island.nil? ? new_island_path : edit_island_path(user_island)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def my_island_selling?(current_user)
-    my_island = Island.find_by(owner: current_user.id)
+    my_island = Island.find_by(user: current_user)
 
     return true unless my_island
 
-    Sale.find_by(island_id: my_island.id)
+    Sale.find_by(island: my_island)
   end
 end
